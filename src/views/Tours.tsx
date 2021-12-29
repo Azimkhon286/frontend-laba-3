77554bb548709ba4441760c525aa6c83bf9dd02c
Azimkhon286 <rahmatulloev_21082001@mail.ru>
2021-12-29 21:18:56 +0900

    make Sightseeing and Tours as array

--- src/views/Tours.tsx
+++ src/views/Tours.tsx
@@ -1,89 +1,58 @@
 import React from 'react';
 import './Tours.css';
 
+const toursArray = [
+    {
+        label: 'Название',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pretium orci at rutrum pellentesque.',
+        price: '10 000',
+    },
+    {
+        label: 'Название',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pretium orci at rutrum pellentesque.',
+        price: '10 000',
+    },
+    {
+        label: 'Название',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pretium orci at rutrum pellentesque.',
+        price: '10 000',
+    },
+    {
+        label: 'Название',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pretium orci at rutrum pellentesque.',
+        price: '10 000',
+    },
+    {
+        label: 'Название',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pretium orci at rutrum pellentesque.',
+        price: '10 000',
+    },
+    {
+        label: 'Название',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pretium orci at rutrum pellentesque.',
+        price: '10 000',
+    },
+]
+
 export default function Tours() {
     return (
         <section className="Tours" id="tours">
             <h2 className="Tours__label">Туры</h2>
             <div className="Tours__content-container">
-                <div className="Tours__card">
-                    <div className="Tours__description-container">
-                        <h5>Название</h5>
-                        <p className="Tours__description">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pretium orci at rutrum
-                            pellentesque.
-                        </p>
+                {toursArray.map((tour, index) => (
+                    <div className="Tours__card" key={index}>
+                        <div className="Tours__description-container">
+                            <h5>{tour.label}</h5>
+                            <p className="Tours__description">
+                                {tour.description}
+                            </p>
+                        </div>
+                        <div className="Tours__info_container">
+                            <p className="Tours__price">От <b>{tour.price}</b> р.</p>
+                            <button>Купить</button>
+                        </div>
                     </div>
-                    <div className="Tours__info_container">
-                        <p className="Tours__price">От <b>10 000</b> р.</p>
-                        <button>Купить</button>
-                    </div>
-                </div>
-                <div className="Tours__card">
-                    <div className="Tours__description-container">
-                        <h5>Название</h5>
-                        <p className="Tours__description">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pretium orci at rutrum
-                            pellentesque.
-                        </p>
-                    </div>
-                    <div className="Tours__info_container">
-                        <p className="Tours__price">От <b>10 000</b> р.</p>
-                        <button>Купить</button>
-                    </div>
-                </div>
-                <div className="Tours__card">
-                    <div className="Tours__description-container">
-                        <h5>Название</h5>
-                        <p className="Tours__description">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pretium orci at rutrum
-                            pellentesque.
-                        </p>
-                    </div>
-                    <div className="Tours__info_container">
-                        <p className="Tours__price">От <b>10 000</b> р.</p>
-                        <button>Купить</button>
-                    </div>
-                </div>
-                <div className="Tours__card">
-                    <div className="Tours__description-container">
-                        <h5>Название</h5>
-                        <p className="Tours__description">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pretium orci at rutrum
-                            pellentesque.
-                        </p>
-                    </div>
-                    <div className="Tours__info_container">
-                        <p className="Tours__price">От <b>10 000</b> р.</p>
-                        <button>Купить</button>
-                    </div>
-                </div>
-                <div className="Tours__card">
-                    <div className="Tours__description-container">
-                        <h5>Название</h5>
-                        <p className="Tours__description">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pretium orci at rutrum
-                            pellentesque.
-                        </p>
-                    </div>
-                    <div className="Tours__info_container">
-                        <p className="Tours__price">От <b>10 000</b> р.</p>
-                        <button>Купить</button>
-                    </div>
-                </div>
-                <div className="Tours__card">
-                    <div className="Tours__description-container">
-                        <h5>Название</h5>
-                        <p className="Tours__description">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pretium orci at rutrum
-                            pellentesque.
-                        </p>
-                    </div>
-                    <div className="Tours__info_container">
-                        <p className="Tours__price">От <b>10 000</b> р.</p>
-                        <button>Купить</button>
-                    </div>
-                </div>
+                ))}
             </div>
         </section>
     );
